eac3c625461c105cebf4523d2ed5c20353281f69
javlocan <javlocan.dev@gmail.com>
2023-09-19 14:27:16 +0200

    First commit

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+// server setup
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
+app.use(express.static(path.dirname(__dirname) + "/public/"));
+
+// variables
+
+const __appdir = path.dirname(__dirname) + "/app";
+
+// routes
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __appdir });
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile("/home/index.html", { root: __appdir });
+});
+//
+app.get("/nav/home", (req, res) => {
+  res.sendFile("/nav/home/index.html", { root: __appdir });
+});
+
+app.get("/nav/about", (req, res) => {
+  res.sendFile("/nav/about/index.html", { root: __appdir });
+});
