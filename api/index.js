b1c851d29d6d247d8effe1257d4155cf89e1800e
javlocan <javlocan.dev@gmail.com>
2023-09-19 14:30:57 +0200

    Vercel integration

--- api/index.js
+++ api/index.js
@@ -16,7 +16,11 @@ app.use(express.static(path.dirname(__dirname) + "/public/"));
 const __appdir = path.dirname(__dirname) + "/app";
 
 // routes
-
+app.get("/api", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.redirect("/");
+});
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __appdir });
 });
